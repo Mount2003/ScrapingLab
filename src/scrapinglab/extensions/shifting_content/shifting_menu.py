3116df478a6b59b90787d0cf907bb389f101a7f2
Maegan <maeganmaulder420@gmail.com>
2026-06-10 11:13:19 +0800

    Implement fix to race condition problem in shifting_menu.py

--- src/scrapinglab/extensions/shifting_content/shifting_menu.py
+++ src/scrapinglab/extensions/shifting_content/shifting_menu.py
@@ -12,6 +12,7 @@ class ShiftingMenu(ShiftingPage):
         mode_link: Locator = page.get_by_role(
             'link', name='click here').nth(2)
         await mode_link.click()
+        await page.wait_for_load_state('domcontentloaded')
         response_url: str = 'https://the-internet.herokuapp.com/portfolio/'
         async with (
             page.expect_response(response_url) as response_info
@@ -20,5 +21,5 @@ class ShiftingMenu(ShiftingPage):
         response: Response|None = await response_info.value
         if response:
             self.logger.info('Shifted portfolio link successfully clicked.')
+            await page.wait_for_load_state('domcontentloaded')
             await page.goto(self.home_url)
-            await page.pause()
